d2f11afdd08b5ec2f059aca67dfd9f9ee4e274d9
olivejua <tmfrl4710@gmail.com>
2022-07-11 23:12:44 +0900

    refactor: db 스키마 각 테이블 id data_type int -> bigint 변경

--- common/src/main/resources/schema.sql
+++ common/src/main/resources/schema.sql
@@ -3,12 +3,12 @@ create table tb_user (
     id varchar(16) not null unique,
     password varchar(50) not null,
     nickname varchar(30) not null,
-    point tinyint not null default 0,
+    point mediumint not null default 0,
     created_date datetime not null default now()
 );
 
 create table tb_store (
-    store_id int primary key,
+    store_id bigint primary key,
     name varchar(50) not null,
     address varchar(120) not null,
     min_order_amount mediumint not null default 0,
@@ -16,25 +16,25 @@ create table tb_store (
     closing_time time not null,
     cooking_time time not null,
     open_status enum('open', 'closed') not null,
-    food_category_id int not null
+    food_category_id bigint not null
 );
 
 create table tb_food_category (
-    food_category_id int primary key,
+    food_category_id bigint primary key,
     name varchar(15) not null
 );
 
 create table tb_menu (
-    menu_id int primary key,
-    store_id int not null,
+    menu_id bigint primary key,
+    store_id bigint not null,
     name varchar(50) not null,
     amount mediumint not null default 0,
     image_urls varchar(255)
 );
 
 create table tb_menu_option (
-    menu_option_id int primary key,
-    menu_id int not null,
+    menu_option_id bigint primary key,
+    menu_id bigint not null,
     title varchar(30) not null,
     selection varchar(50) not null,
     additional_amount mediumint not null default 0,
@@ -42,18 +42,18 @@ create table tb_menu_option (
 );
 
 create table tb_order (
-    order_id int primary key,
-    user_id int not null,
-    store_id int not null,
+    order_id bigint primary key,
+    user_id bigint not null,
+    store_id bigint not null,
     amount int not null,
     status enum('requested', 'cooking', 'completed', 'canceled') not null,
     additional_comment varchar(150),
-    payment_id int not null,
+    payment_id bigint not null,
     created_date datetime not null default now()
 );
 
 create table tb_order_menu (
-    order_menu_id int primary key,
+    order_menu_id bigint primary key,
     order_id int not null,
     menu_id int not null,
     menu_options varchar(150),
@@ -62,7 +62,7 @@ create table tb_order_menu (
 );
 
 create table tb_payment (
-    payment_id int primary key,
+    payment_id bigint primary key,
     amount mediumint not null default 0,
     method enum('card', 'account_transfer') not null,
     created_date datetime not null default now(),
@@ -73,10 +73,10 @@ create table tb_payment (
 );
 
 create table tb_review (
-    review_id int primary key,
-    user_id int not null,
-    store_id int not null,
-    order_id int not null,
+    review_id bigint primary key,
+    user_id bigint not null,
+    store_id bigint not null,
+    order_id bigint not null,
     score tinyint not null,
     content varchar(1000) not null,
     created_date datetime not null,
