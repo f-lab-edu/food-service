95e54bde8451d663c7ea1afab6ba3af89fcba2bf
olivejua <tmfrl4710@gmail.com>
2022-07-12 23:28:08 +0900

    feat: entity repository 생성

--- common/src/main/java/com/food/common/user/repository/UserRepository.java
+++ common/src/main/java/com/food/common/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.food.common.repository;
+package com.food.common.user.repository;
 
-import com.food.common.domain.User;
+import com.food.common.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
