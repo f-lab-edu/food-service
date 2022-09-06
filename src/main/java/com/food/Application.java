8a4d99a7b4c8e6f6f3dff57076dd95a81b5f31a5
olivejua <tmfrl4710@gmail.com>
2022-09-06 21:44:28 +0900

    refactor: root project spring dependency 추가

--- src/main/java/com/food/Application.java
+++ src/main/java/com/food/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
