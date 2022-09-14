ba9b6ff952e9584af522aa772ef74f5e45809d4d
olivejua <tmfrl4710@gmail.com>
2022-09-14 23:46:53 +0900

    test: restdocs 테스트애노테이션 추가

--- src/test/java/com/food/SuperApiTest.java
+++ src/test/java/com/food/SuperApiTest.java
@@ -1,15 +1,21 @@
 package com.food;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
 @Transactional
 public class SuperApiTest {
@@ -19,9 +25,10 @@ public class SuperApiTest {
     protected MockMvc mvc;
 
     @BeforeEach
-    void setup() {
+    void setup(RestDocumentationContextProvider restDocumentation) {
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
                 .apply(springSecurity())
                 .build();
     }
