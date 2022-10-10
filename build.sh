b8b787d3f635be126e72be8d022f7c98bd958baf
olivejua <tmfrl4710@gmail.com>
2022-10-10 14:11:00 +0900

    chore: build.sh 스크립트 생성
    - 모든 모듈 clean build 명령어 추가

---
+++ build.sh
@@ -0,0 +1,13 @@
+cd ./common
+mvn clean package
+cd ../user
+mvn clean package
+cd ../auth
+mvn clean package
+cd ../store
+mvn clean package
+cd ../order
+mvn clean package
+cd ..
+mvn clean test
+mvn package
