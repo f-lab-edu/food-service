84ab123d1c658cefbb2b201931ec6ecf5e1b64b0
olivejua <tmfrl4710@gmail.com>
2022-10-22 15:50:06 +0900

    refactor: Point 엔티티에 payment unique 제약조건 추가 및 type에 회수, 재적립 유형추가

--- common/src/main/java/com/food/common/user/enumeration/PointType.java
+++ common/src/main/java/com/food/common/user/enumeration/PointType.java
@@ -2,7 +2,9 @@ package com.food.common.user.enumeration;
 
 public enum PointType {
     COLLECT("적립"),
-    USE("사용")
+    USE("사용"),
+    RETRIEVE("회수 (적립 취소)"),
+    RECOLLECT("재적립 (사용 취소)")
     ;
 
     private final String description;
